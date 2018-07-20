16434770ce1366fa718dd53dd62b01e4720b36cf
hongyangjia <hongyooo@icloud.com>
2018-07-20 19:15:19 +0800

    java-vioio init

---
+++ contract/src/main/solidity/Sample.sol
@@ -0,0 +1,17 @@
+pragma solidity ^0.4.23;
+
+contract Sample{
+  address public owner;
+  constructor() public{
+   owner = msg.sender;
+  }
+
+  uint storedData;
+   function set(uint x) public {
+          storedData = x*3;
+     }
+
+   function get() public view returns (uint) {
+     return storedData;
+   }
+}
